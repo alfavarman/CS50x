0ab80725178bf51da6fd204cf247624dafffbff6
CKS <communicate2charles@gmail.com>
2023-03-27 01:17:00 +0700

    newsubmission

--- week1/lamas.c
+++ week1/lamas.c
@@ -7,7 +7,7 @@ int main(void)
     int population;
     do
     {
-        population = get_int("Enter initial size of population: ");
+        population = get_int("Enter starting population: ");
     }
     while (population < 9);
 
@@ -15,19 +15,19 @@ int main(void)
     int expected_population;
     do
     {
-        expected_population = get_int("Enter expexted size of population: ");
+        expected_population = get_int("Enter expected population: ");
     }
-    while (expected_population <= population);
+    while (expected_population < population);
 
 
     // TODO: Calculate number of years until we reach threshold
     int years = 0;
     while (population < expected_population)
     {
-        population += population / 12;
+        population += (population / 3) - (population / 4);
         years++;
     }
 
     // TODO: Print number of years
-    printf("To reach expected size of population it will take %i years\n", years);
+    printf("Years: %i", years);
 }
