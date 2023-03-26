9cfa9d531836cfee9186a30fdb1e3fab5188d0bd
CKS <communicate2charles@gmail.com>
2023-03-26 23:00:04 +0700

    submittedt

--- week1/lamas.c
+++ week1/lamas.c
@@ -3,30 +3,31 @@
 
 int main(void)
 {
-    // prompt user for size of population
+    // TODO: Prompt for start size
     int population;
     do
     {
-        population = get_int("Starting population size: ");
-    } while (population < 9);
+        population = get_int("Enter initial size of population: ");
+    }
+    while (population < 9);
 
-
-    // propmpt user for expected population
+    // TODO: Prompt for end size
     int expected_population;
     do
     {
-        expected_population = get_int("Expected population size: ");
-    } while (expected_population <= population);
+        expected_population = get_int("Enter expexted size of population: ");
+    }
+    while (expected_population <= population);
 
 
+    // TODO: Calculate number of years until we reach threshold
     int years = 0;
     while (population < expected_population)
     {
-        population += population/12;
+        population += population / 12;
         years++;
     }
 
-    // print numbers of years rquired
-    printf("years: %i\n", years);
-
+    // TODO: Print number of years
+    printf("To reach expected size of population it will take %i years\n", years);
 }
