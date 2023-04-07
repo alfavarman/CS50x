7f21bd8e422bd4fae423f20e7d0baba3fadcd73b
CKS <communicate2charles@gmail.com>
2023-04-07 12:07:21 +0700

    w5 inheritance and speller

---
+++ week1/w5_data_structures/hashing.c
@@ -0,0 +1,5 @@
+// bucketizing, hashing passing something in and getting anoter value
+// hashing func is algorythm to implement hasing idea (pass item nd received recalculated value)
+
+// hashtables: array of pointers
+// O(n) or O(n/constant)
---
+++ week1/w5_data_structures/tree.c
@@ -0,0 +1,3 @@
+// binary tree should be balanced
+// binary tree from regular tree differ its order CONVENTION (right child grows bigger)
+// binary search in recursion
