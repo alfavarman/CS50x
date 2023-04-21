81513bf54f8641521684d281755da432adce2572
CKS <communicate2charles@gmail.com>
2023-04-22 00:17:24 +0700

    background js main functionality

--- FinalProject/background.js
+++ FinalProject/background.js
@@ -19,18 +19,61 @@ fetch(chrome.runtime.getURL('block_list.csv'))
     });
   });
 
-
+// load blockList
+chrome.storage.sync.get('blockList');
 
 // set extenstion state
+  // listen for action
+chrome.browserAction.onClicked.addListener(function(tab) {
+// set icon acording to state:
+  // get 'switch'
+  chrome.storage.sync.get('switch', function(data) {
+   // define new switch and overwight it
+    var newState = !data.switch;
+    chrome.storage.sync.set({ switch: newState }); 
+    // set icon based on state
+    var iconPath = newState ? 'active128.png' : 'inactive128.png';
+    var title = newState ? 'Focus ON' : 'Focus OFF';
 
-// set icon acording to state
+    // check all tabs in all windows:
+    // Send a message to the content script to update its state
 
-// if state is on
+    chrome.tabs.query({ active: true}, function(tabs) {          // { active: true, currentWindow: true } !SETTING OPTION!
+      chrome.tabs.sendMessage(tabs[0].id, { switch: newState });
+    });
+  });
+});
 
-    //listen the request
+// listen webRequest: to edit or cancell them
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // if switch
+    chrome.storage.sync.get('switch', function(data) {
+      if (data.switch) {
+        // load block_list
+        chrome.storage.sync.get('blockList', function(data) {
+          var blockList = data.blockList;
 
-    // check if reurst is BlockList
+          // check for url in the list
+          for (let i = 0; i < blockList.length; i++) {
+            var url = blockList[i].url;
+            //
+            if (details.url.indexOf(url) !== -1) {
+              // inform url is currently blcock
+              chrome.notifications.create({
+                type: 'basic',
+                title: 'Productivity bloacker',
+                message: 'Access to ' + url + ' has been blocked.',
+              });
+              // cancel-block access
+              return { cancel: true };
+            }
+          }
+        });
+      }
+    });
+  },
+  { urls: ['<all_urls>'] },
+  ['blocking']
+);
 
-        // create notifiction f url: {url} blocked by oriductivityBlocker
-
-        // cancel request
