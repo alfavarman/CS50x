3512aafc3cc13023de6a90c872af16459ba939f6
CKS <communicate2charles@gmail.com>
2023-05-15 14:49:33 +0700

    in/active extension icon

--- FinalProject/v1.0.2/background.js
+++ FinalProject/v1.0.2/background.js
@@ -1,35 +1,56 @@
 // variable for switch
-let isOn = true;
+var isOn = true;
 
-chrome.action.onClicked.addListener((tab) => {
-  console.log("Extension icon clicked");
-  if (tab.button === 0) {
-    // set switch
-    isOn = !isOn;
-    // set icon
+// Load switch state from storage
+chrome.storage.local.get('isOn', function(result) {
+  if (result.isOn !== undefined) {
+    isOn = result.isOn;
     updateIcon(isOn);
-    // inform content script
     updateContentScripts(isOn);
   }
 });
 
-// functions definitions //
 
+chrome.action.onClicked.addListener((tab, info) => {
+  console.log("Extension icon clicked");
+  // set switch
+  isOn = !isOn;
+  // set icon
+  updateIcon(isOn);
+  // inform content script
+  updateContentScripts(isOn);
+  // save switch state to storage
+  chrome.storage.local.set({isOn: isOn});
+  });
+
+// functions definitions //
 function updateIcon(isOn) {
-  const iconPath = isOn ? "./icons/active16.png" : "./icons/inactive16.png";
-  chrome.action.setIcon({ path: iconPath });
+  console.log("updateIcon fun execute with isOn variable: " + isOn);
+  const iconPath = isOn ? {
+    "16": "./icons/active16.png",
+    "32": "./icons/active32.png",
+    "48": "./icons/active48.png",
+    "128": "./icons/active128.png"
+  } : {
+    "16": "./icons/inactive16.png",
+    "32": "./icons/inactive32.png",
+    "48": "./icons/inactive48.png",
+    "128": "./icons/inactive128.png"
+  };
+  chrome.action.setIcon({path: iconPath});
 }
 
-function updateContentScripts(isEnabled) {
-  const contentScripts = chrome.contentScripts;
-  if (isEnabled) {
+function updateContentScripts(isOn) {
+  console.log("updateContentScripts fun execute with isOn variable: " + isOn);
+  const contentScripts = chrome.scripting;
+  if (isOn) {
     contentScripts.register({
       matches: [
         "https://www.facebook.com/*",
         "https://www.netflix.com/*"
       ],
       css: [
-        "./css/block.css"
+        { file: "./css/block.css" }
       ]
     });
   } else {
@@ -39,8 +60,10 @@ function updateContentScripts(isEnabled) {
         "https://www.netflix.com/*"
       ],
       css: [
-        "./css/block.css"
+        { file: "./css/block.css" }
       ]
     });
   }
 }
+
+console.log("Extension loaded");
