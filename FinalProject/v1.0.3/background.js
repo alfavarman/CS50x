c6f0dbc51b5fe4d7ece82e25e3332e0e9efa1244
CKS <communicate2charles@gmail.com>
2023-05-19 01:46:15 +0700

    fix bugs

--- FinalProject/v1.0.3/background.js
+++ FinalProject/v1.0.3/background.js
@@ -1,110 +1,107 @@
-// variable for switch
+// Variable for switch
 var isOn = true;
 
-// global matches
+// Global matches
 var matches = [
   'https://*.facebook.com/*',
   'https://*.netflix.com/*'
 ];
 
-// 
+// Load matches from storage or set default values
 chrome.storage.local.get('matches', function(result) {
   if (result.matches === undefined) {
-    chrome.storage.local.set({matches: matches});
-  }
-  else {
-    matches = result.matches
+    chrome.storage.local.set({ matches: matches });
+  } else {
+    matches = result.matches;
   }
 });
 
-// Load switch state from storage
+// Load switch state from storage or set default value to true
 chrome.storage.local.get('isOn', function(result) {
   if (result.isOn !== undefined) {
     isOn = result.isOn;
-    updateIcon();
-    executeCss();
+  } else {
+    chrome.storage.local.set({ isOn: isOn });
   }
+  updateIcon();
+  updateBadgeText();
 });
 
+// Event listeners
 chrome.tabs.onCreated.addListener((tab) => {
-  // execute if newly open tabs with url.'
-  console.log("tabs.oncreated triggered with tab.url: " + tab.url);
   if (tab.url) {
     executeCss(tab.id);
   }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("tabs.onUpdated triggered with tabId: " + tabId);
-  if (changeInfo.status === "complete") {
+  if (changeInfo.status === 'complete') {
     executeCss(tabId);
   }
 });
 
 chrome.webNavigation.onCommitted.addListener((details) => {
-  console.log("webNavigation.onCommitted triggered");
   executeCss(details.tabId);
 });
 
 chrome.action.onClicked.addListener((tab) => {
-  console.log("Extension icon clicked");
-  // set switch
   isOn = !isOn;
-  // set icon
   updateIcon();
-  // inform content script
+  updateBadgeText();
   executeCss();
-  // save switch state to storage
-  chrome.storage.local.set({isOn: isOn});
-  });
+  chrome.storage.local.set({ isOn: isOn });
+});
 
-// Context menu 
-  chrome.contextMenus.create({
-    id: "options",
-    title: "Options",
-    contexts: ["browser_action"],
-  });
-  
-  // listen for click
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "options") {
-      chrome.tabs.create({ url: "options.html" });
-    }
-  });
-  
-// functions definitions //
+// Context menu
+chrome.contextMenus.create({
+  id: 'options',
+  title: 'Options',
+  contexts: ['browser_action'],
+});
+
+// Listen for click on context menu
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'options') {
+    chrome.tabs.create({ url: 'options.html' });
+  }
+});
+
+// Function to update the icon
 function updateIcon() {
-  console.log("updateIcon fun execute with variable isOn value: " + isOn);
-  const iconPath = isOn ? {
-    "16": "./icons/active16.png",
-    "32": "./icons/active32.png",
-    "48": "./icons/active48.png",
-    "128": "./icons/active128.png"
-  } : {
-    "16": "./icons/inactive16.png",
-    "32": "./icons/inactive32.png",
-    "48": "./icons/inactive48.png",
-    "128": "./icons/inactive128.png"
-  };
+  const iconPath = isOn
+    ? {
+        '16': './icons/active16.png',
+        '32': './icons/active32.png',
+        '48': './icons/active48.png',
+        '128': './icons/active128.png',
+      }
+    : {
+        '16': './icons/inactive16.png',
+        '32': './icons/inactive32.png',
+        '48': './icons/inactive48.png',
+        '128': './icons/inactive128.png',
+      };
 
-  // Retrieve the showBadgeText value from storage
+  chrome.action.setIcon({ path: iconPath });
+}
+
+// Function to update the badge text
+function updateBadgeText() {
+  // Retrieve the showBadgeText value from storage or set default value to true
   chrome.storage.local.get('showBadgeText', function(result) {
     const showBadgeText = result.showBadgeText !== undefined ? result.showBadgeText : true;
 
     // Show or hide the badge text based on the showBadgeText value
-    const badgeText = showBadgeText ? (isOn ? "ON" : "OFF") : '';
+    const badgeText = showBadgeText ? (isOn ? 'ON' : 'OFF') : '';
     chrome.action.setBadgeText({ text: badgeText });
-
-    chrome.action.setIcon({ path: iconPath });
   });
 }
 
 // Example: Execute CSS based on extension status and tab URL changes
 function executeCss(tabId) {
-  
   const cssDetails = {
     target: { url: matches },
-    files: ["./css/block.css"],
+    files: ['./css/block.css'],
   };
 
   const injectCss = (tab) => {
@@ -120,5 +117,4 @@ function executeCss(tabId) {
   });
 }
 
-
-console.log("Extension loaded");
+console.log('Extension loaded');
--- FinalProject/v1.0.3/options.js
+++ FinalProject/v1.0.3/options.js
@@ -1,87 +1,91 @@
 // options.js
 
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleSwitch = document.getElementById("toggle-switch");
-    const blockedUrlsList = document.getElementById("blocked-urls-list");
-    const newUrlInput = document.getElementById("new-url-input");
-    const addUrlButton = document.getElementById("add-url-button");
-  
-    // Load switch state from storage
-    chrome.storage.local.get("showBadgeText", function (result) {
-      if (result.showBadgeText !== undefined) {
-        toggleSwitch.checked = result.showBadgeText;
-      }
+  const toggleSwitch = document.getElementById("toggle-switch");
+  const blockedUrlsList = document.getElementById("blocked-urls-list");
+  const newUrlInput = document.getElementById("new-url-input");
+  const addUrlButton = document.getElementById("add-url-button");
+
+  // Load switch state from storage
+  chrome.storage.local.get("showBadgeText", function (result) {
+    if (result.showBadgeText !== undefined) {
+      toggleSwitch.checked = result.showBadgeText;
+    }
+  });
+
+  toggleSwitch.addEventListener("change", function () {
+    const showBadgeText = toggleSwitch.checked;
+    chrome.storage.local.set({ showBadgeText: showBadgeText });
+    chrome.runtime.sendMessage({ action: "updateBadgeText" });
+  });
+
+  // Load blocked URLs from storage
+  chrome.storage.local.get("matches", function (result) {
+    if (result.matches !== undefined) {
+      const urls = result.matches;
+      displayBlockedUrls(urls);
+    }
+  });
+
+  function displayBlockedUrls(urls) {
+    blockedUrlsList.innerHTML = "";
+    urls.forEach(function (url) {
+      const listItem = document.createElement("li");
+      listItem.textContent = url;
+      const deleteIcon = document.createElement("span");
+      deleteIcon.classList.add("delete-icon");
+      deleteIcon.innerHTML = "&#10006;";
+      deleteIcon.addEventListener("click", function () {
+        deleteUrl(url);
+      });
+      listItem.appendChild(deleteIcon);
+      blockedUrlsList.appendChild(listItem);
     });
-  
-    // Load blocked URLs from storage
+  }
+
+  function deleteUrl(url) {
     chrome.storage.local.get("matches", function (result) {
       if (result.matches !== undefined) {
         const urls = result.matches;
-        displayBlockedUrls(urls);
+        const index = urls.indexOf(url);
+        if (index !== -1) {
+          urls.splice(index, 1);
+          chrome.storage.local.set({ matches: urls }, function () {
+            displayBlockedUrls(urls);
+          });
+        }
       }
     });
-  
-    function displayBlockedUrls(urls) {
-      blockedUrlsList.innerHTML = "";
-      urls.forEach(function (url) {
-        const listItem = document.createElement("li");
-        listItem.textContent = url;
-        const deleteIcon = document.createElement("span");
-        deleteIcon.classList.add("delete-icon");
-        deleteIcon.innerHTML = "&#10006;";
-        deleteIcon.addEventListener("click", function () {
-          deleteUrl(url);
-        });
-        listItem.appendChild(deleteIcon);
-        blockedUrlsList.appendChild(listItem);
-      });
-    }
-  
-    function deleteUrl(url) {
-      chrome.storage.local.get("matches", function (result) {
-        if (result.matches !== undefined) {
-          const urls = result.matches;
-          const index = urls.indexOf(url);
-          if (index !== -1) {
-            urls.splice(index, 1);
+  }
+
+  addUrlButton.addEventListener("click", function () {
+    const newUrl = newUrlInput.value.trim();
+    if (newUrl !== "") {
+      if (isValidUrl(newUrl)) {
+        chrome.storage.local.get("matches", function (result) {
+          if (result.matches !== undefined) {
+            const urls = result.matches;
+            urls.push(newUrl);
             chrome.storage.local.set({ matches: urls }, function () {
               displayBlockedUrls(urls);
+              newUrlInput.value = "";
             });
           }
-        }
-      });
+        });
+      } else {
+        displayErrorMessage("Invalid input");
+      }
     }
-  
-    addUrlButton.addEventListener("click", function () {
-        const newUrl = newUrlInput.value.trim();
-        if (newUrl !== "") {
-          if (isValidUrl(newUrl)) {
-            chrome.storage.local.get("matches", function (result) {
-              if (result.matches !== undefined) {
-                const urls = result.matches;
-                urls.push(newUrl);
-                chrome.storage.local.set({ matches: urls }, function () {
-                  displayBlockedUrls(urls);
-                  newUrlInput.value = "";
-                });
-              }
-            });
-          } else {
-            displayErrorMessage("Invalid input");
-          }
-        }
-      });
-      
-      function isValidUrl(url) {
-        // regular expression to validate the URL format
-        const urlPattern = /^(https?|chrome):\/\/([^\s/$.?#]*\.)?[^\s/$.?#]+\.[^\s/$.?#]+(\/.*)\/$/;
-        return urlPattern.test(url);
-      }
-      
-      function displayErrorMessage(message) {
-        const errorMessage = document.getElementById("error-message");
-        errorMessage.textContent = message;
-      }
-      
   });
-  
+
+  function isValidUrl(url) {
+    // regular expression to validate the URL format
+    const urlPattern = /^(https?|chrome):\/\/([^\s/$.?#]*\.)?[^\s/$.?#]+\.[^\s/$.?#]+(\/.*)\/$/;
+    return urlPattern.test(url);
+  }
+
+  function displayErrorMessage(message) {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.textContent = message;
+  }
+});
