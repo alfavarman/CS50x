84caeccb9fa9478fb77cd82d5d2d7c275186dd81
CKS <communicate2charles@gmail.com>
2023-05-18 21:59:24 +0700

    add/remove url

--- FinalProject/v1.0.3/options.js
+++ FinalProject/v1.0.3/options.js
@@ -1,6 +1,10 @@
+// options.js
+
 document.addEventListener("DOMContentLoaded", function () {
     const toggleSwitch = document.getElementById("toggle-switch");
     const blockedUrlsList = document.getElementById("blocked-urls-list");
+    const newUrlInput = document.getElementById("new-url-input");
+    const addUrlButton = document.getElementById("add-url-button");
   
     // Load switch state from storage
     chrome.storage.local.get("showBadgeText", function (result) {
@@ -9,69 +13,59 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   
-    toggleSwitch.addEventListener("change", function () {
-      const isOn = toggleSwitch.checked;
-  
-      // Save switch state to storage
-      chrome.storage.local.set({ showBadgeText: isOn });
-    });
-  
     // Load blocked URLs from storage
     chrome.storage.local.get("matches", function (result) {
       if (result.matches !== undefined) {
-        result.matches.forEach(function (url) {
-          const li = document.createElement("li");
-          li.textContent = url;
-          blockedUrlsList.appendChild(li);
-        });
+        const urls = result.matches;
+        displayBlockedUrls(urls);
       }
     });
+  
     function displayBlockedUrls(urls) {
-        urls.forEach(function (url) {
-          const listItem = document.createElement("li");
-          listItem.textContent = url;
-          const deleteIcon = document.createElement("span");
-          deleteIcon.classList.add("delete-icon");
-          deleteIcon.innerHTML = "&#10006;";
-          deleteIcon.addEventListener("click", function () {
-            deleteUrl(url);
-          });
-          listItem.appendChild(deleteIcon);
-          blockedUrlsList.appendChild(listItem);
+      blockedUrlsList.innerHTML = "";
+      urls.forEach(function (url) {
+        const listItem = document.createElement("li");
+        listItem.textContent = url;
+        const deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("delete-icon");
+        deleteIcon.innerHTML = "&#10006;";
+        deleteIcon.addEventListener("click", function () {
+          deleteUrl(url);
         });
-      }
-    
-      function deleteUrl(url) {
-        chrome.storage.local.get("blockedUrls", function (result) {
-          if (result.blockedUrls !== undefined) {
-            const urls = result.blockedUrls;
-            const index = urls.indexOf(url);
-            if (index !== -1) {
-              urls.splice(index, 1);
-              chrome.storage.local.set({ blockedUrls: urls }, function () {
-                blockedUrlsList.innerHTML = "";
-                displayBlockedUrls(urls);
-              });
-            }
+        listItem.appendChild(deleteIcon);
+        blockedUrlsList.appendChild(listItem);
+      });
+    }
+  
+    function deleteUrl(url) {
+      chrome.storage.local.get("matches", function (result) {
+        if (result.matches !== undefined) {
+          const urls = result.matches;
+          const index = urls.indexOf(url);
+          if (index !== -1) {
+            urls.splice(index, 1);
+            chrome.storage.local.set({ matches: urls }, function () {
+              displayBlockedUrls(urls);
+            });
+          }
+        }
+      });
+    }
+  
+    addUrlButton.addEventListener("click", function () {
+      const newUrl = newUrlInput.value.trim();
+      if (newUrl !== "") {
+        chrome.storage.local.get("matches", function (result) {
+          if (result.matches !== undefined) {
+            const urls = result.matches;
+            urls.push(newUrl);
+            chrome.storage.local.set({ matches: urls }, function () {
+              displayBlockedUrls(urls);
+              newUrlInput.value = "";
+            });
           }
         });
       }
-    
-      addUrlButton.addEventListener("click", function () {
-        const newUrl = newUrlInput.value.trim();
-        if (newUrl !== "") {
-          chrome.storage.local.get("blockedUrls", function (result) {
-            if (result.blockedUrls !== undefined) {
-              const urls = result.blockedUrls;
-              urls.push(newUrl);
-              chrome.storage.local.set({ blockedUrls: urls }, function () {
-                blockedUrlsList.innerHTML = "";
-                displayBlockedUrls(urls);
-                newUrlInput.value = "";
-              });
-            }
-          });
-        }
-      });
     });
+  });
   
