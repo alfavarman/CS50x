1f901d059037b1ab37dc318aec013c0a3bed5ea4
CKS <communicate2charles@gmail.com>
2023-05-24 17:30:25 +0700

    validator' regex corrected

--- FinalProject/v1.0.3/options.js
+++ FinalProject/v1.0.3/options.js
@@ -81,7 +81,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function isValidUrl(url) {
     // regular expression to validate the URL format
-    const urlPattern = /^(https?|chrome):\/\/([^\s/$.?#]*\.)?[^\s/$.?#]+\.[^\s/$.?#]+(\/.*)\/$/;
+    // shcema http https chrome ftp file *
+    //^(https?|chrome|file|ftp|\*)\:\/\/(\*|\*\.(?!-)[a-z0-9\-]{2,}[a-zA-Z0-9].[a-zA-Z0-9]{2,63}|((?!-)[a-z0-9\-]*[a-zA-Z0-9]\.){1,4}(?!-)[a-z0-9\-]*[a-zA-Z0-9])?\/[^\s{}|\\^~\[\]]*
+    var protocol = /^(https?|chrome|file|ftp|\*);
+    // url with wildchar * - must ve followed by / or dot
+    var host = \:\/\/(\*|\*\.(?!-)[a-z0-9\-]{2,}[a-zA-Z0-9].[a-zA-Z0-9]{2,63}|((?!-)[a-z0-9\-]*[a-zA-Z0-9]\.){1,4}(?!-)[a-z0-9\-]*[a-zA-Z0-9])?;
+    // path exlided with dangerous chars
+    var path = \/[^\s{}|\\^~\[\]]*/;
+    const urlPattern = protocol+host+path;
     return urlPattern.test(url);
   }
 
